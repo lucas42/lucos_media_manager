a2a5820b9a3727f18928cfea2d65a2548226dec5
Luke Blaney <git@lukeblaney.co.uk>
2021-01-11 00:30:37 +0000

    spaces to tabs in NullTrack

--- tests/NullTrack.java
+++ tests/NullTrack.java
@@ -6,24 +6,24 @@ import java.util.HashMap;
 
 class NullTrackTest {
 
-    @Test
-    void allNullTracksAreEqual() {
-    	NullTrack nullTrackA = new NullTrack();
-    	NullTrack nullTrackB = new NullTrack("https://example.com/nulltrack");
-    	NullTrack nullTrackC = new NullTrack("https://example.com/nullytrack", new HashMap<String, String>());
-    	Track realTrack = new Track("https://example.com/track");
-        assertEquals(nullTrackA, nullTrackA);
-        assertEquals(nullTrackA, nullTrackB);
-        assertEquals(nullTrackB, nullTrackC);
-        assertEquals(nullTrackC, nullTrackA);
-        assertNotEquals(nullTrackA, realTrack);
-        assertNotEquals(realTrack, nullTrackB);
-        assertNotEquals(nullTrackC, null);
-    }
-    @Test
-    void hashCodeIsAlwaysZero() {
-    	NullTrack nullTrackA = new NullTrack();
-    	assertEquals(0, nullTrackA.hashCode());
-    }
+	@Test
+	void allNullTracksAreEqual() {
+		NullTrack nullTrackA = new NullTrack();
+		NullTrack nullTrackB = new NullTrack("https://example.com/nulltrack");
+		NullTrack nullTrackC = new NullTrack("https://example.com/nullytrack", new HashMap<String, String>());
+		Track realTrack = new Track("https://example.com/track");
+		assertEquals(nullTrackA, nullTrackA);
+		assertEquals(nullTrackA, nullTrackB);
+		assertEquals(nullTrackB, nullTrackC);
+		assertEquals(nullTrackC, nullTrackA);
+		assertNotEquals(nullTrackA, realTrack);
+		assertNotEquals(realTrack, nullTrackB);
+		assertNotEquals(nullTrackC, null);
+	}
+	@Test
+	void hashCodeIsAlwaysZero() {
+		NullTrack nullTrackA = new NullTrack();
+		assertEquals(0, nullTrackA.hashCode());
+	}
 
 }
