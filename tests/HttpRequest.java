c4b9f71159b09038f30771516945cbe0dfa7075b
Luke Blaney <git@lukeblaney.co.uk>
2021-01-11 20:15:07 +0000

    add some HTTP Request Tests using mocking

---
+++ tests/HttpRequest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+class HttpRequestTest {
+
+	void compareRequestResponse(String request, String responseSnippet) {
+		try {
+			Socket socket = mock(Socket.class);
+			InetAddress mockedAddress = mock(InetAddress.class);
+			when(mockedAddress.getHostName()).thenReturn("test.host");
+			when(socket.getInetAddress()).thenReturn(mockedAddress);
+			HttpRequest httpRequest = new HttpRequest(socket);
+			InputStream input = new StringBufferInputStream(request);
+			when(socket.getInputStream()).thenReturn(input);
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			when(socket.getOutputStream()).thenReturn(output);
+			httpRequest.run();
+			assertTrue(output.toString().contains(responseSnippet), "response snippet ("+responseSnippet+") not found in response:\n"+output.toString());
+		} catch (Exception e) {
+			fail(e);
+		}
+	}
+
+	@Test
+	void notFound() {
+		compareRequestResponse("GET /unknown HTTP/1.1\n", "404 File Not Found");
+	}
+	@Test
+	void robots() {
+		compareRequestResponse("Get /robots.txt HTTP/1.1\n", "Disallow:");
+	}
+
+
+}
